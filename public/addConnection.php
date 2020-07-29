1191f35166700c052af598b89f539f1be73ec62b
Bob Schockweiler <info@webfashion.in>
2020-07-30 01:54:10 +0200

    Add layout

--- public/addConnection.php
+++ public/addConnection.php
@@ -13,14 +13,49 @@ if(isset($_POST["addConnection"])) {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>SSH Dashboard</title>
 
-<a href="index.php">Overview</a>
+    <link rel="stylesheet" href="stylesheets/style.css">
+</head>
+<body>
 
-<form action="" method="POST">
-    <p>Name: <input type="text" name="name"/></p>
-    <p>Host: <input type="text" name="host"/></p>
-    <p>Username: <input type="text" name="username"/></p>
-    <p>Port: <input type="number" name="port"/></p>
-    <p>Password: <input type="password" name="password"/></p>
-    <input type="submit" name="addConnection" value="Add"/>
-</form>
+
+<div class="page">
+    <div class="page-body">
+        <div class="box-content">
+            <h1>Add SSH connection</h1>
+
+            <form action="" method="POST">
+                <p class="form-item">
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" id="name" required>
+                </p>
+                <p class="form-item">
+                    <label for="host">Host:</label>
+                    <input type="text" name="host" id="host" required>
+                </p>
+                <p class="form-item">
+                    <label for="username">Username:</label>
+                    <input type="text" name="username" id="username"
+                ></p>
+                <p class="form-item">
+                    <label for="port">Port:</label>
+                    <input type="number" name="port" id="port" value="22" required>
+                </p>
+                <p class="form-item">
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password">
+                </p>
+                <input type="submit" name="addConnection" value="Add" class="btn">
+                <br>
+                <a href="index.php" class="btn return">Back to verview</a>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -14,32 +14,33 @@ if(isset($_POST["openConnection"])) {
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>SSH Dashboard</title>
 
-<a href="addConnection.php">Add connection</a>
+    <link rel="stylesheet" href="stylesheets/style.css">
+</head>
+<body>
 
-<h1>All connections</h1>
+<div class="page">
+    <div class="page-body">
+        <div class="box-content">
+            <h1>All connections</h1>
 
-<form action="" method="POST">
-    <table>
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Host</th>
-                <th>Username</th>
-                <th>Port</th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($connections as $SSHConnection) { ?>
-                <tr>
-                    <td><?php echo $SSHConnection->name; ?></td>
-                    <td><?php echo $SSHConnection->host; ?></td>
-                    <td><?php echo $SSHConnection->username; ?></td>
-                    <td><?php echo $SSHConnection->port; ?></td>
-                    <td><input style="background: blue; color: white;" type="submit" name="openConnection" value="<?php echo $SSHConnection->name; ?>"></td>
-                </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-</form>
+            <form action="" method="POST">
+                <?php foreach($connections as $SSHConnection) { ?>
+                    <input type="submit" name="openConnection" title="<?php echo $SSHConnection->name; ?>" value="<?php echo $SSHConnection->name; ?>">
+                <?php } ?>
+            </form>
+
+            <a href="addConnection.php" class="btn full-width">+ Add SSH connection</a>
+        </div>
+    </div>
+</div>
+
+
+
+</body>
+</html>
